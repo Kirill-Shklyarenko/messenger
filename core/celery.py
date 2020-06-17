0adb030266f12eddc93a977a28c2081f51c57728
Kirill Shklyarenko <shklyara94@gmail.com>
2020-06-17 07:49:32 +0300

    :sparkles: Add celery & celery-beat

---
+++ core/celery.py
@@ -0,0 +1,23 @@
+import os
+
+from celery import Celery
+
+# set the default Django settings module for the 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+app = Celery('Moex',
+             backend='rpc://',
+             broker='pyamqp://')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+app.conf.beat_schedule = {
+
+    'every minute': {
+        'task': 'Message checker',
+        'schedule': 60.0,
+    },
+
+}
+
+app.autodiscover_tasks()
