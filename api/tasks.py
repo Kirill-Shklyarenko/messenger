07bf0c843908f887e01eef7dbc18e8bb42007af5
Kirill Shklyarenko <shklyara94@gmail.com>
2020-06-19 12:04:59 +0300

    :art: Change tasks logic

--- api/tasks.py
+++ api/tasks.py
@@ -9,9 +9,10 @@ from .models import Message
 
 @shared_task(bind=True,
              name='Send Message',
-             max_retries=3,
-             soft_time_limit=20)
-@transaction.atomic  # Cause DB can lock if several tasks will get same object
+             soft_time_limit=5,
+             ignore_result=True,
+             default_retry_delay=30 * 60,  # retry in 30 minutes
+             autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={'max_retries': 5})
 def send_message_task(self, message_pk):  # noqa
     message = Message.objects.get(pk=message_pk)
     print(f'Try to send {message} with STATUS = {message.status}')
@@ -21,16 +22,18 @@ def send_message_task(self, message_pk):  # noqa
 
 
 @shared_task(bind=True,
-             name='Message checker')
+             name='Message checker',
+             time_limit=60,
+             ignore_result=True)
+@transaction.non_atomic_requests
 def message_checker_task(self):  # noqa
     utc = pytz.UTC
     for message in Message.objects.all():
-        # This for deferred_time
-        if message.deferred_time:
-            deffered_time = message.deferred_time.replace(second=0, microsecond=0, tzinfo=utc)
-            datetime_now = dt.datetime.now().replace(second=0, microsecond=0, tzinfo=utc)
-            if deffered_time <= datetime_now:
-                send_message_task.apply_async(args=(message.pk,))
-        # This for failed messages
         if message.status != 2:
-            send_message_task.apply_async(args=(message.pk,))
+            if message.deferred_time:
+                deffered_time = message.deferred_time.replace(second=0, microsecond=0, tzinfo=utc)
+                datetime_now = dt.datetime.now().replace(second=0, microsecond=0, tzinfo=utc)
+                if deffered_time <= datetime_now:
+                    send_message_task.apply_async(args=(message.pk,))
+            else:
+                send_message_task.apply_async(args=(message.pk,))
